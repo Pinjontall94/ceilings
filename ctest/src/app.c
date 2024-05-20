94ef19d2a1322dc960a98103dd0418a695213f8d
Sam Johnson <sambjohnson94@gmail.com>
2024-05-20 00:23:10 -0700

    working ctest/build/app calling make test

--- ctest/src/app.c
+++ ctest/src/app.c
@@ -1,20 +1,46 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-void run_project()
+void print_div_line(const char character, const int length)
 {
-    char buffer[256] = {0};
-    FILE *fp = popen("./exercise01/app", "r");
-    fgets(buffer, sizeof(buffer), fp);
-    pclose(fp);
+    // Prints a divider line with character specified and for a given length
+    for (int i = 0; i < length; i++)
+        printf("%c", character);
+    printf("\n");
 }
 
-int main(int argc, char *argv[])
+bool print_ascii_file(const char *filename)
 {
-    printf("You entered %d arguments\n", argc);
-    for (int i = 0; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        return false;
     }
 
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), file))
+        printf("%s", buffer);
+
+    fclose(file);
+    return true;
+}
+
+void run_tests()
+{
+    char command[] = "make test";
+    system(command);
+}
+
+int main(void) 
+{
+    if (!print_ascii_file("../assets/asciilogo.txt")) 
+        return 1;
+    if (!print_ascii_file("../assets/asciiart3.txt")) 
+        return 1;
+    print_div_line('=', 80);
+    run_tests();
     return 0;
 }
