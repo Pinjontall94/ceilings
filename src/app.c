fa081117da7bc76d60a06412d1a072f08f6a3b00
Sam Johnson <sambjohnson94@gmail.com>
2024-09-24 10:27:45 -0700

    update app to use combined logo and art, add color codes

--- src/app.c
+++ src/app.c
@@ -1,8 +1,16 @@
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+
+#define ANSI_COLOR_RED		"\x1b[31m"
+#define ANSI_COLOR_GREEN	"\x1b[32m"
+#define ANSI_COLOR_YELLOW	"\x1b[33m"
+#define ANSI_COLOR_BLUE		"\x1b[34m"
+#define ANSI_COLOR_MAGENTA	"\x1b[35m"
+#define ANSI_COLOR_CYAN		"\x1b[36m"
+#define ANSI_COLOR_RESET	"\x1b[0m"
 
 void print_div_line(const char character, const int length)
 {
@@ -12,10 +20,18 @@ void print_div_line(const char character, const int length)
     printf("\n");
 }
 
+void print_colored_div(const char *color, const char character, const int length)
+{
+    printf("%s", color);
+    print_div_line(character, length);
+    printf(ANSI_COLOR_RESET);
+}
+
 bool print_ascii_file(const char *filename)
 {
     FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         fprintf(stderr, "%s\n", strerror(errno));
         return false;
     }
@@ -30,15 +46,13 @@ bool print_ascii_file(const char *filename)
 
 void run_tests()
 {
-    char command[] = "make test";
+    const char command[] = "make test 2>/dev/null";
     system(command);
 }
 
-int main(void) 
+int main(void)
 {
-    if (!print_ascii_file("../assets/asciilogo.txt")) 
-        return 1;
-    if (!print_ascii_file("../assets/asciiart3.txt")) 
+    if (!print_ascii_file("../assets/ascii_ceilings.txt"))
         return 1;
     print_div_line('=', 80);
     run_tests();
