2d52fb72cd3946aa315b937ae7ee720ef8709a69
Sam Johnson <sambjohnson94@gmail.com>
2024-11-28 21:10:15 -0800

    improved 004_loops challenges, add do-while

--- exercises/004_loops.c
+++ exercises/004_loops.c
@@ -1,20 +1,31 @@
+#include <stdio.h>
+
 // C has two looping constants you've almost certainly seen before:
 //   - while loops
 //   - for loops
+// aaand the less common do-while
 
 int main()
 {
     int counter = 5;
-    ____ (counter _ 200)
+    ____ (counter < 20)
     {
         // How would you make this complete in half the number of steps?
-        counter = ____;
+        ____;
+        printf("while loop counter: %i\n", counter);
     }
 
-    for (int i = 0; i <= 200; i__)
+    for (int i = 0; i <= 20; i += 2)  // what if this line is `for (;;)`?
     {
-        counter__;
+	printf("for loop counter: %i\n", counter);
+        ____;
     }
 
+    ____ {
+	// [challenge]: change loop to print this line only once!
+	printf("do whiles are weird! counter: %i\n", counter);
+	____;
+    } ____ (counter > 0);
+    
     return counter;
 }
